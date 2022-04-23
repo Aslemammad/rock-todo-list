7e494fbe439939c6a2ea23500bf7723006371e2a
Sajad Sharhani <sharhani.sajad@gmail.com>
2022-04-24 01:49:59 +0430

    google analytics

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Provider as SessionProvider } from 'next-auth/client'
-import { PageContextProvider } from './usePageContext'
-import type { PageContext } from './types'
-import { Link } from './Link'
-import { Session } from 'next-auth'
+import React, { useEffect } from "react";
+import { Provider as SessionProvider } from "next-auth/client";
+import { PageContextProvider } from "./usePageContext";
+import type { PageContext } from "./types";
+import { Link } from "./Link";
+import { Session } from "next-auth";
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-204034316-1"; // OUR_TRACKING_ID
+ReactGA.initialize(process.env.TRACKING_ID as string);
 
-export { PageShell }
+export { PageShell };
 
 function PageShell({
   pageContext,
   children,
 }: {
-  pageContext: PageContext & { session: Session | null }
-  children: React.ReactNode
+  pageContext: PageContext & { session: Session | null };
+  children: React.ReactNode;
 }) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <React.StrictMode>
       <SessionProvider session={pageContext.session || undefined}>
@@ -29,5 +35,5 @@ function PageShell({
         </PageContextProvider>
       </SessionProvider>
     </React.StrictMode>
-  )
+  );
 }
--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -1,26 +1,28 @@
-import ReactDOMServer from 'react-dom/server'
-import React from 'react'
-import { escapeInject } from 'vite-plugin-ssr'
-import { PageShell } from './PageShell'
-import { getPageTitle } from './getPageTitle'
-import type { PageContext } from './types'
-import type { PageContextBuiltIn } from 'vite-plugin-ssr'
-import { Session } from 'next-auth'
+import ReactDOMServer from "react-dom/server";
+import React from "react";
+import { escapeInject } from "vite-plugin-ssr";
+import { PageShell } from "./PageShell";
+import { getPageTitle } from "./getPageTitle";
+import type { PageContext } from "./types";
+import type { PageContextBuiltIn } from "vite-plugin-ssr";
+import { Session } from "next-auth";
 
-export { render }
-export { passToClient }
+export { render };
+export { passToClient };
 
-const passToClient = ['pageProps', 'documentProps', 'session']
+const passToClient = ["pageProps", "documentProps", "session"];
 
-function render(pageContext: PageContextBuiltIn & PageContext & { session: Session | null }) {
-  const { Page, pageProps } = pageContext
+function render(
+  pageContext: PageContextBuiltIn & PageContext & { session: Session | null }
+) {
+  const { Page, pageProps } = pageContext;
   const stream = ReactDOMServer.renderToNodeStream(
     <PageShell pageContext={pageContext}>
       <Page {...pageProps} />
-    </PageShell>,
-  )
+    </PageShell>
+  );
 
-  const title = getPageTitle(pageContext)
+  const title = getPageTitle(pageContext);
 
   return escapeInject`<!DOCTYPE html>
     <html>
@@ -30,5 +32,5 @@ function render(pageContext: PageContextBuiltIn & PageContext & { session: Sessi
       <body>
         <div id="page-view">${stream}</div>
       </body>
-    </html>`
+    </html>`;
 }
